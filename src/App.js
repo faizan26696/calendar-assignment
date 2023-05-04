17c99ea18408bf37de416b210e8504a982ca9b6f
Mohammad Faizan <111674496+faizan26696@users.noreply.github.com>
2023-05-05 00:58:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Calendar from "./Component/calendar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
